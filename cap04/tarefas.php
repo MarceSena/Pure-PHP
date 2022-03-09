f5a001dbb35af53b18e447e30d70a928dbea6470
MarceSena <marcelinosena@outlook.com>
2022-03-09 17:06:43 -0400

    capitulo 4 - Entrada de dados com formulário

--- cap04/tarefas.php
+++ cap04/tarefas.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <html>
   <head>
-    <title>Gerenciador de Tarefas</title>
+    <title>Gerenciador de Tarefas </title>
   </head>
   <body>
     <h1>Gerenciador de Tarefas</h1>
